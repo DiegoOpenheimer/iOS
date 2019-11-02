e48ef964f60036982e0db7ba704ea63467cd3133
Diego <diegoop.pa@hotmail.com>
2019-11-02 09:59:30 -0300

    Adicionado teclado do tipo date picker para cadastro de ano de validade

--- travel/travel/DetailsPackage/controller/DetailsPackageViewController.swift
+++ travel/travel/DetailsPackage/controller/DetailsPackageViewController.swift
@@ -14,6 +14,7 @@ class DetailsPackageViewController: UIViewController {
     var packageTravel: PackageTravel?
     @IBOutlet var image: UIImageView?
     
+    @IBOutlet weak var textFieldDate: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         if let package = packageTravel {
@@ -21,9 +22,24 @@ class DetailsPackageViewController: UIViewController {
         }
         super.hideKeyboardWhenTappedAround()
         KeyboardAvoiding.avoidingView = self.view
+        initializeTextFieldDate()
     }
 
     @IBAction func popPage(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    
+    private func initializeTextFieldDate() {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        textFieldDate.inputView = datePicker
+        datePicker.addTarget(self, action: #selector(eventValueChangeDatePicker(sender:)), for: .valueChanged)
+    }
+    
+    @objc func eventValueChangeDatePicker(sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MM yyyy"
+        textFieldDate.text = dateFormatter.string(from: sender.date)
     }
 }
